c4dea49ad51b881227be2d864db56a828e0a9db8
Madalin Ignisca <madalin.ignisca@gmail.com>
2016-05-07 14:38:15 +0100

    created static property for the roman numerals

--- src/RomanNumeralsConverter.php
+++ src/RomanNumeralsConverter.php
@@ -3,7 +3,15 @@
 class RomanNumeralsConverter
 {
 
-    
+    public static $romanNumerals = [
+      1 => 'I',
+      5 => 'V',
+      10 => 'X',
+      50 => 'L',
+      100 => 'C',
+      500 => 'D',
+      1000 => 'M',
+    ];
 
     public function convert($number) {
 
