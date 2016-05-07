a5a586dd29ef96e908b71ee1b8bda2c48c00f1e4
Madalin Ignisca <madalin.ignisca@gmail.com>
2016-05-07 22:45:05 +0100

    the most simple numbers pass

--- spec/RomanNumeralsConverterSpec.php
+++ spec/RomanNumeralsConverterSpec.php
@@ -16,4 +16,19 @@ class RomanNumeralsConverterSpec extends ObjectBehavior
     {
         $this->convert(3000)->shouldReturn('MMM');
     }
+
+    function it_calculates_the_roman_numeral_for_3300()
+    {
+        $this->convert(3300)->shouldReturn('MMMCCC');
+    }
+
+    function it_calculates_the_roman_numeral_for_3330()
+    {
+        $this->convert(3330)->shouldReturn('MMMCCCXXX');
+    }
+
+    function it_calculates_the_roman_numeral_for_3333()
+    {
+        $this->convert(3333)->shouldReturn('MMMCCCXXXIII');
+    }
 }
